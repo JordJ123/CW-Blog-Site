d8059e944de576ff87b7b0462381b1858d536afd
JordJ123 <71386858+JordJ123@users.noreply.github.com>
2021-11-20 16:31:15 +0000

    Displays posts
    Posts index page displays posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,7 +9,7 @@ class Post extends Model
 {
     use HasFactory;
 
-    public function account() {
+    public function user() {
         return $this->belongsTo('App\Models\User');
     }
 
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,5 +3,10 @@
 @section('title', 'Feed')
 
 @section('content')
-    
+    @foreach ($posts as $post)
+        <div>
+            <p><b>{{ $post->user()->first()->name }}</b><p>
+            <p>{{ $post->text }}</p>
+        </div>
+    @endforeach
 @endsection
