86d51fb39ba7de6304a29eab3b9a2b5cd497e109
JordJ123 <71386858+JordJ123@users.noreply.github.com>
2021-11-20 17:02:24 +0000

    Displays Likes
    Posts and comments display the amount of likes that they have

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,10 +5,17 @@
 @section('content')
     @foreach ($posts as $post)
         <div>
-            <p><b>{{ $post->user()->first()->name }}</b><p>
+            <p><b>
+                {{ $post->user()->first()->name }}
+                (Likes {{ $post->likes()->count() }})
+            </b><p>
             <p>{{ $post->text }}</p>
             @foreach ($post->comments()->get() as $comment)
-                <p>{{ $comment->user()->first()->name }}: {{ $comment->text}}</p>
+                <p>
+                    {{ $comment->user()->first()->name }}
+                    (Likes {{ $comment->likes()->count() }}): 
+                    {{ $comment->text}}
+                </p>
             @endforeach
         </div>
     @endforeach
