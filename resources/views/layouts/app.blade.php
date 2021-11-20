40022ef0ccdd907b7088bde8945d648a8450ff41
JordJ123 <71386858+JordJ123@users.noreply.github.com>
2021-11-20 16:19:36 +0000

    Post Controller Index Method
    PostController's index method can now be accessed by the routes

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+
+    <head>
+        <title>Posts 'R' Us - @yield('title')</title>
+    </head>
+    
+    <body>
+        <div>
+            <h1>@yield('title')</h1>
+        </div>
+        <div>
+            @yield('content')    
+        </div>
+    </body>
+
+</html>
