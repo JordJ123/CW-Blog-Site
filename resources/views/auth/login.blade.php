ce990700621fb62b261ec5671b38487b9fe3c33c
JordJ123 <71386858+JordJ123@users.noreply.github.com>
2021-11-23 17:13:27 +0000

    Login to Register
    Added the ability to go the register page from the login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,6 +41,12 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                    {{ __("Don't have an account?") }}
+                </a>
+
+                &#160&#160&#160
+
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
