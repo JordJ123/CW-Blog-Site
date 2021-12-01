864df4d842ca83c1304f191b30fd6284d751a3a7
JordJ123 <71386858+JordJ123@users.noreply.github.com>
2021-12-01 00:44:39 +0000

    Post Edit
    Can edit a post

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -3,8 +3,9 @@
 @section('title', 'Edit Post')
 
 @section('content')
-    <form method="PUT" action="{{ route('posts.update', ['id' => $post->id]) }}">
+    <form method="POST" action="{{ route('posts.update', ['id' => $post->id]) }}">
         @csrf
+        @method('PUT')
         <input type="text" name="text" value="{{ $post->text }}"><br><br>
         <input type="submit" value="Update"><br><br>
     </form>
