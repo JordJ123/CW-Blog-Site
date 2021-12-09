b1623e9d7acedfc990c3688c8123a8daabc6f1c4
JordJ123 <71386858+JordJ123@users.noreply.github.com>
2021-12-09 21:41:32 +0000

    Forgot View CSS

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,36 +1,23 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.account')
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+@section('title', "Forgot Password")
+
+@section('form')
+    
+    <form method="POST" action="{{ route('password.email') }}">
+        @csrf
+        <div class="mb-3">
+            <label for="email" class="form-label text-secondary">Email address</label>
+            <input type="email" class="form-control" id="email" name="email" 
+                value="{{ old ('email') }}" required>
         </div>
+        <div class="text-center">
+            <button type="submit" class="btn btn-secondary">Email Password Link</button>
+        </div>
+        <div class="text-center">
+            <a class="btn link-dark " href="{{ route('login') }}">
+                Remembered your Password?</a>
+        </div> 
+    </form>
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+@endsection
