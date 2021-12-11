43ddc4315f8103c87e50b7a080f7ac4bc197a2cd
JordJ123 <71386858+JordJ123@users.noreply.github.com>
2021-12-11 15:11:09 +0000

    Users isAdmin Attribute
    Added if the user is an administrator to the users table

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -17,31 +17,36 @@ class UserTableSeeder extends Seeder
         $jordan = new User;
         $jordan->name = "Jordan123";
         $jordan->email = "jordan@email.com";
-        $jordan->password = "Password123!";
+        $jordan->password = "password";
+        $jordan->isAdmin = true;
         $jordan->save();
 
         $bob = new User;
         $bob->name = "Bob123";
         $bob->email = "bob@email.com";
         $bob->password = "Password123!";
+        $bob->isAdmin = false;
         $bob->save();
 
         $steve = new User;
         $steve->name = "Steve23";
         $steve->email = "steve@email.com";
         $steve->password = "Password123!";
+        $steve->isAdmin = false;
         $steve->save();
 
         $susan = new User;
         $susan->name = "Susan123";
         $susan->email = "susan@email.com";
         $susan->password = "Password123!";
+        $susan->isAdmin = false;
         $susan->save();
 
         $jane = new User;
         $jane->name = "Jane123";
         $jane->email = "jane@email.com";
         $jane->password = "Password123!";
+        $jane->isAdmin = false;
         $jane->save();
 
         $users = User::factory()->count(10)->create();
